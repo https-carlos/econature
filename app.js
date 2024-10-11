1a7290f1daee23ce5101d51941c056f720aa1077
aks10-ifal <140767449+aks10-ifal@users.noreply.github.com>
2024-10-11 01:17:17 -0300

    Update and rename catalogo.css to app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const sequelize = require('./Config/database');
+const authRoutes = require('./Routes/authRoutes');
+
+const app = express();
+const port = 3000;
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'chave-secreta',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 * 60 },
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.static('archives'));
+
+app.use('/', authRoutes);
+
+sequelize.sync()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta ${port}`);
+        });
+    })
+    .catch(err => {
+        console.error('Erro ao sincronizar banco de dados:', err);
+    });
