6edfdcbfb029be379cc905e8a781bfc98efc7b88
aks10-ifal <140767449+aks10-ifal@users.noreply.github.com>
2024-10-11 01:35:31 -0300

    atualizando database.js para o azure

--- Config/database.js
+++ Config/database.js
@@ -7,9 +7,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    port: process.env.DB_PORT, 
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    }
   }
 );
-
 
 module.exports = sequelize;
